6729ccf36afd508e1d30240f854602010e3da5ed
Andy Feidje Joseph <ingandy75@gmail.com>
2019-05-31 14:02:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-
+def initialize(id=nil,name,grade)
+  @name = name
+  @grade = grade
+end
 end
